0893371527e09eb27f599845582d717b6d8641c1
Jhonatan Figueiredo Almeida <jhonatan1406@users.noreply.github.com>
2023-08-20 21:16:39 -0300

    tela usuário

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,56 @@
-function toggleNav() {
-    const navItems = document.querySelector('.nav-items');
-    navItems.classList.toggle('show');
-    document.querySelector('.navbar').classList.toggle('show');
-  
-    const menuIcon = document.querySelector('.menu-icon');
-    menuIcon.classList.toggle('rotate');
-    menuIcon.classList.toggle('align');
-    
-    const navLinks = document.querySelectorAll('.nav-items li a');
-  
-    const miniNavItems = document.querySelector('.mini-nav-items');
-    miniNavItems.classList.toggle('hide');
-  
-    if (navItems.classList.contains('show')) {
-      navLinks.forEach(link => link.style.fontSize = 'medium');
-    } else {
-      navLinks.forEach(link => link.style.fontSize = 'small');
-    }
-  };
-  
+
+
+const carouselItems = document.querySelectorAll('.json-item');
+let currentItem = 0;
+
+function showItem(index) {
+  carouselItems.forEach(item => {
+    item.classList.remove('active');
+  });
+
+  carouselItems[index].classList.add('active');
+}
+
+function nextItem() {
+  currentItem = (currentItem + 1) % carouselItems.length;
+  showItem(currentItem);
+}
+
+function previousItem() {
+  currentItem = (currentItem - 1 + carouselItems.length) % carouselItems.length;
+  showItem(currentItem);
+}
+
+
+// ... restante do código ...
+
+document.addEventListener('DOMContentLoaded', () => {
+  showItem(currentItem);
+
+  const nextButton = document.createElement('button');
+  nextButton.innerText = '>';
+  nextButton.className = 'carousel-button-next-button';
+  nextButton.addEventListener('click', nextItem);
+
+  const previousButton = document.createElement('button');
+  previousButton.innerText = '<';
+  previousButton.className = 'carousel-button-previous-button';
+  previousButton.addEventListener('click', previousItem);
+
+  const indicatorsContainer = document.getElementById('indicators');
+  for (let i = 0; i < carouselItems.length; i++) {
+    const indicator = document.createElement('div');
+    indicator.className = 'carousel-indicator';
+    indicator.addEventListener('click', () => {
+      currentItem = i;
+      showItem(currentItem);
+    });
+    indicatorsContainer.appendChild(indicator);
+  }
+
+
+  document.getElementById('json-carousel').appendChild(previousButton);
+  document.getElementById('json-carousel').appendChild(nextButton);
+});
+
+
